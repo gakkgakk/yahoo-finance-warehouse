036b09ab8fafe99dae934795d40e857ad1d99540
Vegar Skaret <vskaret@mykolab.com>
2022-10-11 20:06:18 +0200

    Add quarterly types to balance stream

--- source_yahoo_finance_financials/streams/balance_statement.py
+++ source_yahoo_finance_financials/streams/balance_statement.py
@@ -4,12 +4,22 @@ from .abstract_financial_stream import AbstractFinancialStream
 
 from .constants import BALANCE_TYPE_PARAMETERS
 
+
 class BalanceStatement(AbstractFinancialStream):
     url_base = "https://query2.finance.yahoo.com/ws/fundamentals-timeseries/v1/finance/timeseries/"
 
-    def path(self, *, stream_state: Mapping[str, Any] = None, stream_slice: Mapping[str, Any] = None, next_page_token: Mapping[str, Any] = None) -> str:
+    def path(
+        self,
+        *,
+        stream_state: Mapping[str, Any] = None,
+        stream_slice: Mapping[str, Any] = None,
+        next_page_token: Mapping[str, Any] = None,
+    ) -> str:
         next_index = next_page_token or 0
 
-        type_string = "%2C".join(BALANCE_TYPE_PARAMETERS)
+        annual_types = ["annual" + element for element in BALANCE_TYPE_PARAMETERS]
+        quarterly_types = ["quarterly" + element for element in BALANCE_TYPE_PARAMETERS]
+
+        type_string = "%2C".join(annual_types + quarterly_types)
 
         return f"{self.tickers[next_index]}?lang=en-US&region=US&type={type_string}&merge=false&period1=493590046&period2=1664481730"
